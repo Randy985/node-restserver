6e884e3a1a1e8e37d9acc3596065cce99173cb79
Randy985 <r.eleaz.lop@gmail.com>
2022-08-26 08:55:44 -0600

    first commir

---
+++ controllers/usuarios.js
@@ -0,0 +1,40 @@
+const { response, request } = require('express');
+const request = require('request');
+
+const usuariosGet = (req = request, res = response) => {
+    const {q, nombre, apikey} = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q, nombre, apikey
+    })
+}
+
+const usuariosPost = (req, res = response) => {
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre, edad
+    })
+}
+const usuariosPut = (req, res = response) => {
+    const {id} = req.params;
+
+    res.json({
+        msg: 'put API - controlador',
+        id
+    })
+}
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    })
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete
+}
